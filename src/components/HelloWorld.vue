794e98910e72467a2cf480dbb0c499ef73e45c87
Kojokyeibadu <kojokyeibadu@gmail.com>
2026-05-18 01:17:18 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://o1mh35kwm8.execute-api.us-east-2.amazonaws.com/default/lambda1'
+const API_ENDPOINT = 'https://c4jtyx9n6d.execute-api.us-east-2.amazonaws.com/default/lambda1'
 
 export default {
   name: 'app',
